fe912044a32bb2f557383f8d99812917c8cf579d
Nader Zaman <naderzaman50@gmail.com>
2024-01-05 19:36:24 +0500

    Update Array.js

---
+++ my-app/vite-project/JS-Refresher/Array.js
@@ -0,0 +1,21 @@
+// JavaScript Array
+// Array is a special variable, which can hold more than one value.
+// Why use Array ??
+// if u have list of items(a list of fruits,users etc ) storing a fruit in a single varibale will look like this 
+let fruit1 = 'mango';
+let fruit2 = 'banana';
+let fruit3 = 'orange';
+// Accessing array elements 
+// you can access an array element by reffering to its index number 
+// const fruits = ['apple','mango','banana'];
+// let fruit = fruits[0];
+// console.log(fruit);
+// Converting an Array into string
+// JS method toString() converts an array into string of comma separated array values.
+const fruits = ['apple','mango','banana'];
+console.log(typeof(fruits));
+console.log(fruits);
+const strFruits = fruits.toString();
+console.log(typeof(strFruits));
+console.log(strFruits);
+
