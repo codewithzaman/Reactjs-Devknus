33125d18150d9bb1e3797f1e89eecf12c3837705
Nader Zaman <naderzaman50@gmail.com>
2024-01-05 13:35:57 +0500

    Update JS-StringMethods.js

--- my-app/vite-project/JS-Refresher/JS-StringMethods.js
+++ my-app/vite-project/JS-Refresher/JS-StringMethods.js
@@ -1,5 +1,11 @@
 // String SubString(indexStart,indexEnd)
 let fruits = 'apple, mango,banana';
 console.log(fruits.substring(7,12));
-// String replace (pattren,replace)
-
+// String replace (pattren,replacement)
+let text = 'Please visit Microsoft';
+let newText = text.replace('Microsoft','Devknus')
+console.log(newText);
+// String replaceAll (pattren,replacement)
+let texts = "I love cats,cats are good animal,i have 5 cats";
+let newTexts = texts.replaceAll('cats','dogs');
+console.log(newTexts);
