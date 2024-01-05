567cd5821f06b2a33ba69fa16e7f62f8333af20c
Nader Zaman <naderzaman50@gmail.com>
2024-01-05 14:18:43 +0500

    Created Numbers,js

---
+++ my-app/vite-project/JS-Refresher/Numbers.js
@@ -0,0 +1,5 @@
+// JavaScript Numbers 
+ // javaScript has only one type of number . Numbers can be written with or without decimal.
+ // let x = 3.14 >> A number with decimals
+ // let x = 3 >> A number without decimals
+ // JavaScript Number methods 
